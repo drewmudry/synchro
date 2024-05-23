fa103511e25905dfa36325b60b45d1d09d465da9
drewmudry <91819824+drewmudry@users.noreply.github.com>
2024-05-22 22:40:59 -0700

    add extra selectable tab

--- app/_components/topbar.tsx
+++ app/_components/topbar.tsx
@@ -1,7 +1,6 @@
 "use client";
-
 import { UserButton } from "@clerk/clerk-react";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { OrganizationSwitcher } from "@clerk/clerk-react";
 
 export const Topbar = ({ selectedSpan, setSelectedSpan }) => {
@@ -11,7 +10,7 @@ export const Topbar = ({ selectedSpan, setSelectedSpan }) => {
 
   return (
     <div className="flex items-center bg-[#813D58] px-4 py-2">
-      <div className="w-1/6 flex justify-start">
+      <div className="w-1/8 flex justify-start">
         <OrganizationSwitcher
           hidePersonal={true}
           appearance={{
@@ -21,34 +20,35 @@ export const Topbar = ({ selectedSpan, setSelectedSpan }) => {
           }}
         />
       </div>
-      <div className="w-1/8"></div>
-      <div className="w-1/6 flex justify-end">
+      <div className="w-1/6"></div>
+      <div className="w-1/2 flex justify-center">
         <span
-          className={`text-[#F5EBDE] cursor-pointer px-2 py-1 rounded ${
-            selectedSpan === "Boards" ? "text-[#661437] bg-[#F5EBDE]" : ""
-          }`}
-          onClick={() => handleSpanClick("Boards")}
-        >
+          className={`text-[#F5EBDE] cursor-pointer px-2 py-1 rounded ${selectedSpan === "Home" ? "text-[#661437] bg-[#F5EBDE] " : ""
+            }`}onClick={() => handleSpanClick("Home")}>
+          Home
+        </span>
+      </div>
+
+      <div className="w-1/2 flex justify-start">
+        <span
+          className={`text-[#F5EBDE] cursor-pointer px-2 py-1 rounded ${selectedSpan === "Boards" ? "text-[#661437] bg-[#F5EBDE] " : ""
+            }`}onClick={() => handleSpanClick("Boards")}>
           Boards
         </span>
       </div>
-      <div className="w-1/6 flex justify-center">
+
+      <div className="w-1/2 flex justify-center">
         <span
-          className={`text-[#F5EBDE] cursor-pointer px-2 py-1 rounded ${
-            selectedSpan === "Tasks" ? "text-[#661437] bg-[#F5EBDE] " : ""
-          }`}
-          onClick={() => handleSpanClick("Tasks")}
-        >
+          className={`text-[#F5EBDE] cursor-pointer px-2 py-1 rounded ${selectedSpan === "Tasks" ? "text-[#661437] bg-[#F5EBDE] " : ""
+            }`}onClick={() => handleSpanClick("Tasks")}>
           Tasks
         </span>
       </div>
-      <div className="w-1/6 flex justify-start">
+
+      <div className="w-1/2 flex justify-start">
         <span
-          className={`text-[#F5EBDE] cursor-pointer px-2 py-1 rounded ${
-            selectedSpan === "Notes" ? "text-[#661437] bg-[#F5EBDE] " : ""
-          }`}
-          onClick={() => handleSpanClick("Notes")}
-        >
+          className={`text-[#F5EBDE] cursor-pointer px-2 py-1 rounded ${selectedSpan === "Notes" ? "text-[#661437] bg-[#F5EBDE] " : ""
+            }`}onClick={() => handleSpanClick("Notes")}>
           Notes
         </span>
       </div>
