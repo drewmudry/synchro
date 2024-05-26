1bd6439119c6e500776531a551e6b931ee742385
drewmudry <91819824+drewmudry@users.noreply.github.com>
2024-05-26 01:59:56 -0700

    change organization button on EmptyOrg component

--- app/dashboard/_components/EmptyOrg.tsx
+++ app/dashboard/_components/EmptyOrg.tsx
@@ -23,7 +23,7 @@ export const EmptyOrg = () => {
                 <Dialog>
                     <DialogTrigger asChild>
                         <Button className="size-lg bg-rose-900">
-                            Organizations
+                            Add Organization
                         </Button>
                     </DialogTrigger>
                     <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
