27b1b6bd18c70b2b8d507507c7be00bd829c62ad
drewmudry <91819824+drewmudry@users.noreply.github.com>
2024-05-26 21:40:50 -0700

    add icons to sidebar

--- app/dashboard/_components/sidebar.tsx
+++ app/dashboard/_components/sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { OrganizationSwitcher } from "@clerk/clerk-react";
+import Image from "next/image";
 
 
 type SidebarProps = {
@@ -52,18 +53,39 @@ export const Sidebar: React.FC<SidebarProps> = ({
           className={`flex items-center text-[#F5EBDE] cursor-pointer px-2 py-3 rounded mb-2 
           ${selectedSpan === 'Boards' ? "bg-[#F5EBDE] text-pink-900" : "hover:bg-[#661437]"}`}
           onClick={() => handleSpanClick("Boards")}>
+            <Image
+            src="/boards.svg"
+            alt="Boards logo"
+            width={20}
+            height={20}
+            className={`mr-4 ${selectedSpan === "Boards" ? "fill-black" : "fill-white"}`}
+          />
           <span className="ml-2">Boards</span>
         </div>
         <div
           className={`flex items-center text-[#F5EBDE] cursor-pointer px-2 py-3 rounded mb-2 
           ${selectedSpan === 'Tasks' ? "bg-[#F5EBDE] text-pink-900" : "hover:bg-[#661437]"}`}
           onClick={() => handleSpanClick("Tasks")}>
+            <Image
+            src="/tasks.svg"
+            alt="tasks logo"
+            width={30}
+            height={30}
+            className={`mr-4 ${selectedSpan === "Tasks" ? "fill-black" : "fill-white"}`}
+          />
           <span className="ml-2">Tasks</span>
         </div>
         <div
           className={`flex items-center text-[#F5EBDE] cursor-pointer px-2 py-3 rounded mb-2 
           ${selectedSpan === 'Notes' ? "bg-[#F5EBDE] text-pink-900" : "hover:bg-[#661437]"}`}
           onClick={() => handleSpanClick("Notes")}>
+            <Image
+            src="/notes.svg"
+            alt="notes logo"
+            width={20}
+            height={20}
+            className={`mr-4 ${selectedSpan === "Notes" ? "fill-black" : "fill-white"}`}
+          />
           <span className="ml-2">Notes</span>
         </div>
         <div
